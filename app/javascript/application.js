9b0cca12e500a73745bc7542058f92b397613219
yosaddis <email.to.yoseph@gmail.com>
2023-10-08 06:32:15 +0300

    Added application js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Greetings from './components/Greetings';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 function App() {
-  return (<h1>Hello World!</h1>);
+  const router = createBrowserRouter([
+    {
+      path: '',
+      element: <Greetings />,
+    },
+  ]);
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
